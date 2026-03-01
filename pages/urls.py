580c31779db4d1c912ff09fc19503fcabfdd9efd
cadasaso <cadasaso@gmail.com>
2026-03-01 17:15:45 -0500

    tarea del tutorial3

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import HomePageView, AboutPageView, ContactPageView
-from .views import ProductIndexView, ProductShowView, ProductCreateView
+from .views import ProductIndexView, ProductShowView, ProductCreateView,CartRemoveAllView,CartView,ImageViewFactory 
 from . import views
+from .utils import ImageLocalStorage
+
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
@@ -10,6 +12,11 @@ urlpatterns = [
     path("contact/", ContactPageView.as_view(), name="contact"),
 
     path("products/", ProductIndexView.as_view(), name="products_index"),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
     path("products/create/", ProductCreateView.as_view(), name="products_create"),
     path("products/<int:id>/", ProductShowView.as_view(), name="products_show"),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from .models import Product,Comment
 from django.views.generic import TemplateView, ListView
 from django.shortcuts import render, redirect, get_object_or_404
+from .utils import ImageLocalStorage
 
 def show(request):
     products = Product.objects.all()
@@ -97,3 +98,62 @@ class ProductCreateView(View):
         viewData["title"] = "Create product"
         viewData["form"] = form
         return render(request, self.template_name, viewData)
+
+class CartView(View):
+    template_name = 'cart/index.html'
+    
+    def get(self, request):
+        # Simulated database for products
+        products = {}
+        products[121] = {'name': 'Tv samsung', 'price': '1000'}
+        products[11] = {'name': 'Iphone', 'price': '2000'}
+
+        # Get cart products from session
+        cart_products = {}
+        cart_product_data = request.session.get('cart_product_data', {})
+
+        for key, product in products.items():
+            if str(key) in cart_product_data.keys():
+                cart_products[key] = product
+
+        # Prepare data for the view
+        view_data = {
+            'title': 'Cart - Online Store',
+            'subtitle': 'Shopping Cart',
+            'products': products,
+            'cart_products': cart_products
+        }
+
+        return render(request, self.template_name, view_data)
+
+    def post(self, request, product_id):
+        # Get cart products from session and add the new product
+        cart_product_data = request.session.get('cart_product_data', {})
+        cart_product_data[product_id] = product_id
+        request.session['cart_product_data'] = cart_product_data
+
+        return redirect('cart_index')
+
+
+class CartRemoveAllView(View):
+    def post(self, request):
+        # Remove all products from cart in session
+        if 'cart_product_data' in request.session:
+            del request.session['cart_product_data']
+
+        return redirect('cart_index')
+
+def ImageViewFactory(image_storage):
+    class ImageView(View):
+        template_name = 'images/index.html'
+
+        def get(self, request):
+            image_url = request.session.get('image_url', '')
+            return render(request, self.template_name, {'image_url': image_url})
+
+        def post(self, request):
+            image_url = image_storage.store(request)
+            request.session['image_url'] = image_url
+            return redirect('image_index')
+
+    return ImageView
